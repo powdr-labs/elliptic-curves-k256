854ab17ca055d1af9475d56b4151b0cf6d44de82
ShuangWu121 <shuang.wu@posteo.net>
2025-07-22 14:10:41 +0300

    add more operations

--- k256/src/arithmetic/affine_op.rs
+++ k256/src/arithmetic/affine_op.rs
@@ -1,28 +1,40 @@
-use crate::FieldElement;
+use crate::{AffinePoint, FieldElement};
+use crate::arithmetic::mul::{G1, G2, MINUS_B1, MINUS_B2, MINUS_LAMBDA, Radix16Decomposition};
+use crate::arithmetic::projective::ENDOMORPHISM_BETA;
 use crate::arithmetic::scalar::{Scalar, WideScalar};
-use core::ops::Add;
+use core::ops::{Add, Neg, Mul};
+use core::usize;
+use elliptic_curve::subtle::{Choice, ConditionallySelectable, ConstantTimeEq};
 use elliptic_curve::{ops::LinearCombination, scalar::IsHigh};
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq,Default)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Default)]
 pub struct PowdrAffinePoint {
     x: FieldElement,
     y: FieldElement,
-    infinity: bool,
+    infinity: u8,
 }
 
 impl Add<PowdrAffinePoint> for PowdrAffinePoint {
-    type Output = Option<PowdrAffinePoint>;
+    type Output = PowdrAffinePoint;
 
-    fn add(self, other: PowdrAffinePoint) -> Option<PowdrAffinePoint> {
-        self.x.normalize();
-        self.y.normalize();
-        other.x.normalize();
-        other.y.normalize();
+    fn add(self, other: PowdrAffinePoint) -> PowdrAffinePoint {
+        if self.infinity != 0 {
+            return other;
+        }
+        if other.infinity != 0 {
+            return self;
+        }
+
+        if other.x == self.x {
+            if self.y == other.y {
+                return self.double(); // assumes self.double() never returns None here
+            } else {
+                //  x1 == x2 but y1 != y2 → vertical line → point at infinity
+                return PowdrAffinePoint::IDENTITY;
+            }
+        }
 
         let dx = (other.x - self.x).normalize();
-        if dx.is_zero().into() {
-            return None;
-        }
         let invert = dx.invert().unwrap();
 
         let dy = other.y - self.y;
@@ -36,21 +48,51 @@ impl Add<PowdrAffinePoint> for PowdrAffinePoint {
         let x3 = lambda.square() - self.x - other.x;
         let y3 = lambda * (self.x + x3.negate(5)) - self.y;
 
-        Some(PowdrAffinePoint {
+        PowdrAffinePoint {
             x: x3.normalize(),
             y: y3.normalize(),
-            infinity: false,
-        })
+            infinity: 0,
+        }
+    }
+}
+
+impl Neg for PowdrAffinePoint {
+    type Output = PowdrAffinePoint;
+
+    fn neg(self) -> PowdrAffinePoint {
+        PowdrAffinePoint::neg(&self)
+    }
+}
+
+impl Mul<Scalar> for PowdrAffinePoint {
+    type Output = PowdrAffinePoint;
+
+    fn mul(self, other: Scalar) -> PowdrAffinePoint {
+        mul(&self, &other)
     }
 }
 
 impl PowdrAffinePoint {
-    pub fn double(self) -> Option<PowdrAffinePoint> {
+    /// Additive identity of the group: the point at infinity.
+    pub const IDENTITY: Self = Self {
+        x: FieldElement::ZERO,
+        y: FieldElement::ZERO,
+        infinity: 1,
+    };
+
+    /// Base point of secp256k1.
+    pub const GENERATOR: Self = Self {
+        x: AffinePoint::GENERATOR.x,
+        y: AffinePoint::GENERATOR.y,
+        infinity: 0,
+    };
+
+    pub fn double(self) -> PowdrAffinePoint {
         let x = self.x.normalize();
         let y = self.y.normalize();
 
         if y.is_zero().into() {
-            return None;
+            return PowdrAffinePoint::IDENTITY;
         }
 
         let num = FieldElement::from(3u64) * x.square();
@@ -60,22 +102,124 @@ impl PowdrAffinePoint {
         let x3 = lambda.square() - FieldElement::from(2u64) * x;
         let y3 = lambda * (x + x3.negate(3)) - y;
 
-        Some(PowdrAffinePoint {
+        PowdrAffinePoint {
             x: x3.normalize(),
             y: y3.normalize(),
-            infinity: false,
-        })
+            infinity: 0,
+        }
+    }
+
+    fn neg(&self) -> Self {
+        PowdrAffinePoint {
+            x: self.x,
+            y: self.y.negate(1).normalize_weak(),
+            infinity: self.infinity,
+        }
+    }
+
+    /// Calculates `k * G`, where `G` is the generator, using precomputed tables.
+    // TODO: can use precomputed tables for better performance
+    pub(super) fn mul_by_generator(k: &Scalar) -> PowdrAffinePoint {
+        PowdrAffinePoint::GENERATOR * *k
+    }
+
+    fn conditional_select(a: &Self, b: &Self, choice: Choice) -> Self {
+        PowdrAffinePoint {
+            x: FieldElement::conditional_select(&a.x, &b.x, choice),
+            y: FieldElement::conditional_select(&a.y, &b.y, choice),
+            infinity: choice.unwrap_u8(),
+        }
+    }
+
+    /// Calculates SECP256k1 endomorphism: `self * lambda`.
+    pub fn endomorphism(&self) -> Self {
+        Self {
+            x: self.x * &ENDOMORPHISM_BETA,
+            y: self.y,
+            infinity: self.infinity,
+        }
     }
 }
 
-fn lincomb(points_and_scalars: &[(PowdrAffinePoint, Scalar); N]) -> PowdrAffinePoint {
+#[inline(always)]
+fn mul(x: &PowdrAffinePoint, k: &Scalar) -> PowdrAffinePoint {
+    lincomb(&[(*x, *k)])
+}
+
+fn lincomb<const N: usize>(
+    points_and_scalars: &[(PowdrAffinePoint, Scalar); N],
+) -> PowdrAffinePoint {
     let mut tables = [(LookupTable::default(), LookupTable::default()); N];
     let mut digits = [(
         Radix16Decomposition::<33>::default(),
         Radix16Decomposition::<33>::default(),
     ); N];
 
-    lincomb(points_and_scalars, &mut tables, &mut digits)
+    lincomb_pippenger(points_and_scalars, &mut tables, &mut digits)
+}
+
+fn lincomb_pippenger(
+    xks: &[(PowdrAffinePoint, Scalar)],
+    tables: &mut [(LookupTable, LookupTable)],
+    digits: &mut [(Radix16Decomposition<33>, Radix16Decomposition<33>)],
+) -> PowdrAffinePoint {
+    xks.iter().enumerate().for_each(|(i, (x, k))| {
+        let (r1, r2) = decompose_scalar(k);
+        let x_beta = x.endomorphism();
+        let (r1_sign, r2_sign) = (r1.is_high(), r2.is_high());
+
+        let (r1_c, r2_c) = (
+            Scalar::conditional_select(&r1, &-r1, r1_sign),
+            Scalar::conditional_select(&r2, &-r2, r2_sign),
+        );
+
+        tables[i] = (
+            LookupTable::from(&PowdrAffinePoint::conditional_select(x, &-*x, r1_sign)),
+            LookupTable::from(&PowdrAffinePoint::conditional_select(
+                &x_beta, &-x_beta, r2_sign,
+            )),
+        );
+
+        digits[i] = (
+            Radix16Decomposition::<33>::new(&r1_c),
+            Radix16Decomposition::<33>::new(&r2_c),
+        )
+    });
+
+    let mut acc = PowdrAffinePoint::IDENTITY;
+    for component in 0..xks.len() {
+        let (digit1, digit2) = digits[component];
+        let (table1, table2) = tables[component];
+
+        acc = table1.select(digit1.0[32])+acc;
+        acc = table2.select(digit2.0[32])+acc;
+    }
+
+    for i in (0..32).rev() {
+        for _j in 0..4 {
+            acc = acc.double();
+        }
+
+        for component in 0..xks.len() {
+            let (digit1, digit2) = digits[component];
+            let (table1, table2) = tables[component];
+
+            acc = table1.select(digit1.0[i])+acc;
+            acc = table2.select(digit2.0[i])+acc;
+        }
+    }
+    acc
+}
+
+/// Find r1 and r2 given k, such that r1 + r2 * lambda == k mod n.
+fn decompose_scalar(k: &Scalar) -> (Scalar, Scalar) {
+    // these _vartime calls are constant time since the shift amount is constant
+    let c1 = WideScalar::mul_shift_vartime(k, &G1, 384) * MINUS_B1;
+    let c2 = WideScalar::mul_shift_vartime(k, &G2, 384) * MINUS_B2;
+    let r2 = c1 + c2;
+    let r1 = k + r2 * MINUS_LAMBDA;
+
+    (r1, r2)
 }
 
 #[derive(Copy, Clone, Default)]
@@ -85,9 +229,29 @@ impl From<&PowdrAffinePoint> for LookupTable {
     fn from(p: &PowdrAffinePoint) -> Self {
         let mut points = [*p; 8];
         for j in 0..7 {
-            points[j + 1] = (p.clone() + points[j].clone()).unwrap();
+            points[j + 1] = p.clone() + points[j].clone();
         }
         LookupTable(points)
+    }
+}
+
+impl LookupTable {
+    /// Given -8 <= x <= 8, returns x * p in constant time.
+    fn select(&self, x: i8) -> PowdrAffinePoint {
+        debug_assert!((-8..=8).contains(&x));
+
+        if x == 0 {
+            PowdrAffinePoint::IDENTITY
+        } else {
+            let abs = x.abs() as usize;
+            let mut point = self.0[abs - 1];
+
+            if x < 0 {
+                point.y = -point.y;
+            }
+
+            point
+        }
     }
 }
 
@@ -97,6 +261,11 @@ mod tests {
     use crate::FieldBytes;
     use hex_literal::hex;
     use sha2::digest::typenum::Double;
+    use elliptic_curve::{
+        Field, Group,
+        rand_core::{OsRng, TryRngCore},
+    };
+
     #[test]
     fn test_addition_double() {
         let x1: FieldElement = FieldElement::from_bytes(
@@ -173,20 +342,28 @@ mod tests {
         let point1 = PowdrAffinePoint {
             x: x1,
             y: y1,
-            infinity: false,
+            infinity: 0,
         };
         let point2 = PowdrAffinePoint {
             x: x2,
             y: y2,
-            infinity: false,
+            infinity: 0,
         };
 
         let addition = point1 + point2.clone();
         let double = point2.double();
-        assert_eq!(addition.as_ref().unwrap().x, x3);
-        assert_eq!(addition.unwrap().y, y3);
+        assert_eq!(addition.x, x3);
+        assert_eq!(addition.y, y3);
 
-        assert_eq!(double.as_ref().unwrap().x, x4.normalize());
-        assert_eq!(double.unwrap().y, y4.normalize());
+        assert_eq!(double.x, x4.normalize());
+        assert_eq!(double.y, y4.normalize());
+    }
+
+    #[test]
+    fn test_mul_by_generator() {
+        let k = Scalar::random(&mut OsRng.unwrap_mut());
+        let reference = PowdrAffinePoint::GENERATOR * k;
+        let test = PowdrAffinePoint::mul_by_generator(&k);
+        assert_eq!(reference, test);
     }
 }
