23fcc54cb60866dae1c8f96f6701c8390328f737
ShuangWu121 <47602565+ShuangWu121@users.noreply.github.com>
2025-07-31 20:56:39 +0200

    Update field.rs
    inv to self

--- k256/src/arithmetic/field.rs
+++ k256/src/arithmetic/field.rs
@@ -176,7 +176,7 @@ impl FieldElement {
             let repr = self.0.0;
             let inv_repr = powdr_openvm_hints_guest::hint_k256_inverse_field_10x26(repr);
             let inv = Self(FieldElementImpl(inv_repr));
-            let normalizes_to_zero = inv.normalizes_to_zero();
+            let normalizes_to_zero = self.normalizes_to_zero();
             if !bool::from(normalizes_to_zero) {
                 // prove its the inverse
                 assert_eq!((inv * self).normalize(), Self::ONE);
